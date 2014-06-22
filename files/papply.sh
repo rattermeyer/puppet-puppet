25bf26ffa3a5e4aa538244c42d127b4e9cc0a901
Richard Attermeyer <richard.attermeyer@gmail.com>
2014-06-22 13:16:05 +0200

    hiera added

--- files/papply.sh
+++ files/papply.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo puppet apply /opt/puppet/manifests/site.pp --modulepath=/opt/puppet/modules/ $*
+sudo puppet apply /opt/puppet/manifests/site.pp --modulepath=/opt/puppet/modules/ --hiera_config=/opt/puppet/hiera.yaml $*
